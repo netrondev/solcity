e9382b4f2235cf3f71847248eb886986b3610f52
Tiaan-Broodryk <tiaanbroodryk44@gmail.com>
2023-07-18 15:53:20 +0200

    background

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -9,11 +9,67 @@ export default function Homepage() {
       <section className=" h-screen w-full bg-gray-900/50">
         {" "}
         <Image
-          src={"/assets/images/solcitybg.png"}
+          src={"/assets/images/solcitybg2.png"}
           alt={""}
-          width={800 * 4}
-          height={600 * 4}
+          width={1000 * 4}
+          height={1000 * 4}
           className="absolute -z-10 h-screen w-screen  bg-white   object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg1.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute -z-10 ml-20 mt-20 h-10 w-10 animate-pulse object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg1.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute -z-10 ml-40 mt-80 h-5 w-5 animate-bounce object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg2.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute -z-10 ml-60 mt-64 h-5 w-5 animate-bounce overflow-hidden object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg2.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute -z-10 ml-72 mt-20 h-10 w-10 animate-bounce overflow-hidden object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg2.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute -z-10 ml-72 mt-96 h-10 w-10 animate-pulse overflow-hidden object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg2.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute bottom-10 -z-10 ml-40 h-10 w-10 animate-pulse overflow-hidden object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg2.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute bottom-6 -z-10 ml-12 h-5 w-5 animate-bounce overflow-hidden object-cover "
+        />
+        <Image
+          src={"/assets/images/starbg1.png"}
+          alt={""}
+          width={1000 * 4}
+          height={1000 * 4}
+          className="absolute bottom-16 -z-10 ml-72 h-5 w-5 animate-bounce overflow-hidden object-cover "
         />
         <NavBar />
         <Draw />
