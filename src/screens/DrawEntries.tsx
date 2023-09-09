8658f80814d1b504ce96dfd728babc852dc509da
Rouan van der Ende <rouan@8bo.org>
2023-09-09 20:10:59 +0200

    improve draw entries list

--- src/screens/DrawEntries.tsx
+++ src/screens/DrawEntries.tsx
@@ -16,6 +16,7 @@ import { Section } from "~/components/Section";
 import { Table } from "~/components/Table";
 import { type RouterOutputs, api } from "~/utils/api";
 import { cn } from "~/utils/cn";
+import { BiLinkExternal, BiCheck, BiCross, BiTime } from "react-icons/bi";
 
 export function DrawEntries(props: {
   draw: RouterOutputs["solcity"]["draws"]["list"][number];
@@ -27,37 +28,46 @@ export function DrawEntries(props: {
   return (
     <Section>
       <Heading>Entries</Heading>
-      <p className="text-xs opacity-50">
-        This shows the transaction history for your this draw wallet.
-      </p>
+      <p className="text-xs opacity-50">The entries for this draw.</p>
       <Table
         data={history.data ?? []}
         columns={[
           {
-            accessorKey: "signature",
-            cell: (p) => (
-              <Link
-                href={`https://solscan.io/tx/${p.row.original.signature}`}
-              >{`${p.row.original.signature.slice(
-                0,
-                5
-              )}...${p.row.original.signature.slice(-5)}`}</Link>
-            ),
+            accessorKey: "confirmationStatus",
+            header: "",
+            cell: (c) => {
+              const confirmationStatus = c.row.original.confirmationStatus;
+              if (confirmationStatus === "finalized")
+                return <BiCheck className="text-emerald-500" />;
+              if (confirmationStatus === "confirmed")
+                return <BiTime className="text-sky-500" />;
+              if (confirmationStatus === "processed")
+                return <BiTime className="text-amber-500" />;
+              return <BiTime className="text-neutral-500" />;
+            },
           },
+          // {
+          //   accessorKey: "signature",
+          //   cell: (p) => (
+          //     <Link
+          //       href={`https://solscan.io/tx/${p.row.original.signature}`}
+          //     >{`${p.row.original.signature.slice(
+          //       0,
+          //       5
+          //     )}...${p.row.original.signature.slice(-5)}`}</Link>
+          //   ),
+          // },
           {
             accessorKey: "date",
             cell: (p) => moment(p.row.original.date).fromNow(),
           },
+          // {
+          //   accessorKey: "fee",
+          //   header: "Fee",
+          //   cell: (p) => p.row.original.details.meta?.fee,
+          // },
           {
-            accessorKey: "confirmationStatus",
-          },
-          {
-            accessorKey: "fee",
-            header: "Fee",
-            cell: (p) => p.row.original.details.meta?.fee,
-          },
-          {
-            accessorKey: "totalChangeAmount",
+            accessorKey: "SOL",
             accessorFn: (i) => {
               return i.totalChangeAmount / LAMPORTS_PER_SOL;
             },
